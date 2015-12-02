0a6a703ba9ece7433c1b642a3dc3e07941b46b6a
AnastasiiaKorosteleva <korosteleva-a@mail.ru>
2015-12-03 00:48:27 +0300

    first commit

--- homeworks/homework5/task4.py
+++ homeworks/homework5/task4.py
@@ -2,16 +2,14 @@ __author__ = 'anastasiiakorosteleva'
 import sys
 data = sys.stdin.read().split("\n")
 import re
-string = 0
-for i in data:
+for index,i in enumerate(data):
     sharp = i.lstrip()
     if sharp.startswith('#'):
-        string += 1
+        continue
     else:
         ids = re.findall(' *([\w,. ]+) = ', i)
-        string += 1
         if len(ids) > 0:
-            print(string, ''.join(ids[0].split(',')))
+            print(index+1, ''.join(ids[0].split(',')))
 
 
 
