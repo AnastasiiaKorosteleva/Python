85abd2e154c31927f7a29195df07994b312f18fb
AnastasiiaKorosteleva <korosteleva-a@mail.ru>
2016-03-06 02:29:44 +0300

    first commit

---
+++ homeworks/springtime'16/homework2/task2.py
@@ -0,0 +1,59 @@
+__author__ = 'anastasiiakorosteleva'
+author__ = 'anastasiiakorosteleva'
+n = int(input())
+class_list = list()
+for k in range(n):
+    class_list.append(input())
+q = int(input())
+query_list = list()
+for l in range(q):
+    query_list.append(input())
+
+
+
+print(class_list)
+print(query_list)
+print(query_list[0])
+
+
+
+# class_dict = {}
+#
+for i in class_list:
+    if ":" in i:
+        if i[0] not in class_dict:
+            if len(i) <= 5:
+                class_dict[i[0]] = i[4]
+                # print(class_dict)
+            if len(i) > 5:
+                class_dict.setdefault(i[0],[i[4]]).append(i[6])
+print(class_dict)
+query_dict = {}
+for i in query_list:
+    query_dict[i.split()[0]] = i.split()[1]
+#     inv_d = {v:k for k, v in query_dict.items()}
+# print(inv_d)
+print(query_dict)
+
+# list_que_val = list(query_dict.values())
+# list_que_keys = list(query_dict.keys())
+# list_classdict_val = list(class_dict.values())
+# # list_classdict_key = list(class_dict.keys())
+# print(list_que_val)
+# print(list_que_keys)
+
+# for i in query_dict.keys():
+#     for j in class_dict.values():
+#         if i == j:
+#             print("ya")
+
+def dfs(graph, start):
+    visited, stack = set(), [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
+    return visited
+
+
