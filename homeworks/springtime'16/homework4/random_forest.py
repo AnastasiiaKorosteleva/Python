91f528a02b881edd06351964c05e28e53b019e89
AnastasiiaKorosteleva <korosteleva-a@mail.ru>
2016-05-01 18:13:03 +0300

    hw4

--- homeworks/springtime'16/homework4/random_forest.py
+++ homeworks/springtime'16/homework4/random_forest.py
@@ -3,6 +3,11 @@ __author__ = 'anastasiiakorosteleva'
 import numpy as np
 from scipy.ndimage import imread
 import os
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics.classification import accuracy_score
 
 def get_images(dir_name):
     images = []
@@ -32,32 +37,23 @@ train_y = np.array((np.ones(200), np.zeros(200)))
 train_y.shape = (400, )
 
 def four_algorythms(algorythm, n_est):
-    from sklearn.ensemble import RandomForestClassifier
     if algorythm == "RandomForestClassifier":
         prediction = RandomForestClassifier(n_estimators=n_est)
-
-    from sklearn.ensemble import ExtraTreesClassifier
     if algorythm == "ExtraTreesClassifier":
         prediction = ExtraTreesClassifier(n_estimators=n_est)
-
-    from sklearn.ensemble import AdaBoostClassifier
     if algorythm == "AdaBoostClassifier":
         prediction = AdaBoostClassifier(n_estimators=n_est)
-
-    from sklearn.ensemble import GradientBoostingClassifier
     if algorythm == "GradientBoostingClassifier":
         prediction = GradientBoostingClassifier(n_estimators=n_est)
 
     prediction = prediction.fit(train, train_y)
     validate_y = np.concatenate((np.ones(50), np.zeros(50)))
     predicted_y = prediction.predict(validate)
-
-    from sklearn.metrics.classification import accuracy_score
     return(accuracy_score(validate_y, predicted_y))
 
 print('\t'+'n_estimators')
-s = ["Algorythm", 10, 20, 40, 80, 100, 150, 200, 300, 400, 500, 1000]
-print('\t'.join(map(str, s)))
+number_est = ["Algorythm", 10, 20, 40, 80, 100, 150, 200, 300, 400, 500, 1000]
+print('\t'.join(map(str, number_est)))
 for algorythm in ["RandomForestClassifier",
                   "ExtraTreesClassifier",
                   "AdaBoostClassifier",
